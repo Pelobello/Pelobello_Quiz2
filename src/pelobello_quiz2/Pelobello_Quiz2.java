7a4e0ceb93706ef661210ffcff2a9fb8fc9f55ac
USER <USER@DAZZLE>
2023-09-22 16:59:58 +0800

    Pelobello_Quiz2

---
+++ src/pelobello_quiz2/Pelobello_Quiz2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pelobello_quiz2;
+
+
+public class Pelobello_Quiz2 {
+
+    public static void main(String[] args) {
+      
+        
+        
+    }
+    
+}
